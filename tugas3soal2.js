cf743f6c7f9e462fea1de4ee7173a06723fb1410
HanNPC <86652190+HanNPC@users.noreply.github.com>
2021-07-28 21:21:08 +0700

    tugas3

---
+++ tugas3soal2.js
@@ -0,0 +1,7 @@
+    var jualMobil = {
+
+    iklan:function(){
+        document.write(BU, saya$(this.nama), jualMobil$(thismerek), tahun$(thistahun), bekas)
+    }
+    }
+    console.log('BU,', jualMobil.saya$ = 'saya Tompel ,',jualMobil.jualMobil$ = ' jualmobil Avanza ,' , jualMobil.tahun$ = 'tahun ' + 2017 + ',' , 'bekas')
---
+++ tugas3soal3.js
@@ -0,0 +1,22 @@
+function arrayToObject(fullName, firstName , lastName, gender, age ){
+    var abduh = {}
+    abduh.fullName = fullName,
+    abduh.firstName = firstName,
+    abduh.lastName = lastName,
+    abduh.gender = gender,
+    abduh.age = age
+    return abduh
+}
+console.log(arrayToObject ('Abduh Muhammad' , 'Abduh', 'Muhammad', 'male', 28 ))
+
+
+function arrayToObject(fullName, firstName , lastName, gender, age ){
+    var taufik = {}
+    taufik.fullName = fullName,
+    taufik.firstName = firstName,
+    taufik.lastName = lastName,
+    taufik.gender = gender,
+    taufik.age = age
+    return taufik
+}
+console.log(arrayToObject ('Ahmad Taufik' , 'Ahmad', 'Taufik', 'male', 35 ))
